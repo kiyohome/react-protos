a7d8e9b33d07acfe8e5ab71d0657ceaded296232
kiyohome <kiyohito.itoh@gmail.com>
2022-04-26 22:30:41 +0900

    静的解析を行うESLintとコードをフォーマットするPrettierを導入

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 # react-protos
 
-プロト作成をすぐに始められるようにReactベースで多くのSPAに共通しそうな機能をセットアップしたサンプルプロジェクトです。
+プロト作成をすぐに始められるように React ベースで多くの SPA に共通しそうな機能をセットアップしたサンプルプロジェクトです。
 
 ## 含めたもの
-- ビルドツール、JSフレームワーク、プログラミング言語
+
+- ビルドツール、JS フレームワーク、プログラミング言語
   - [Vite](https://vitejs.dev/)
   - [React](https://reactjs.org/)
   - [TypeScript](https://www.typescriptlang.org/)
   - 参考にしたもの
     - [Vite で最速 React & TypeScript](https://zenn.dev/sprout2000/articles/98145cf2a807b1)
+- 静的解析ツール、コードフォーマッター
+  - [ESLint](https://eslint.org/)
+  - [Prettier](https://prettier.io/)
+  - 参考にしたもの
+    - [Vite + React の環境構築](https://zenn.dev/kk6/scraps/36fa4579df6acb)
+    - [prettier,eslintを導入する際にハマったこと2021新年](https://zenn.dev/ryusou/articles/nodejs-prettier-eslint2021)
 
 ## これから含めるもの
 
-- 静的解析ツール、コードフォーマッター
 - ルーティング
 - ステート管理
 - 非同期処理のハンドリング
-- UIコンポーネント
+- UI コンポーネント
 - バリデーション
 - 多言語対応
 - モックライブラリ
