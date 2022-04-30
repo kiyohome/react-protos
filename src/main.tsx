c5c582c73f0b63f404a3b2d4f806631172cc34a3
kiyohome <kiyohito.itoh@gmail.com>
2022-05-01 05:32:14 +0900

    ESLintのno-non-null-assertionルールに対応（行コメントのルール無効化を削除）

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import RouterConfig from './RouterConfig';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <MantineProvider
       theme={{
